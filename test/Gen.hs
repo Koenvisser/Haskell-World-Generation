f17257793c7804a24ee29724fdcd5cde24903564
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2024-03-18 22:33:16 +0100

    added test, doesn't pass yet

--- test/Gen.hs
+++ test/Gen.hs
@@ -9,20 +9,18 @@ import Gen.WaveFuncCollapse
 
 test_WaveFuncCollapse :: [TestTree]
 test_WaveFuncCollapse = [
-    testProperty "All positions are placed" (ioProperty prop_allPosArePlaced)
+    testProperty "All positions are placed" (ioProperty allPosArePlaced)
     ]
 
 instance Arbitrary Tile where
     arbitrary = do
         textureLoc <- arbitrary
-        rules <- arbitrary
+        rules <- arbitrary >>= \result -> return $ Rule (\_ _ -> result) 
         charRep <- arbitrary
         return $ Tile textureLoc rules charRep
 
 instance Arbitrary Rule where
-    arbitrary = do
-        rule <- arbitrary
-        return $ Rule rule
+    arbitrary = arbitrary >>= \result -> return $ Rule (\_ _ -> result)
 
 instance Arbitrary TileMap where
     arbitrary = do
@@ -31,8 +29,9 @@ instance Arbitrary TileMap where
 
 instance Arbitrary RuleResult where
     arbitrary = do
-        result <- arbitrary
-        return $ result
+        b <- arbitrary :: Gen Bool
+        f <- arbitrary `suchThat` (\f -> f >= 0 && f <= 1) :: Gen Float
+        elements [CanPlace b, ChancePlace f]
 
 instance CoArbitrary RuleResult where
     coarbitrary (CanPlace b) = coarbitrary b
@@ -42,17 +41,31 @@ instance CoArbitrary TileMap where
     coarbitrary (TileMap tileMap) = coarbitrary tileMap
 
 instance CoArbitrary Tile where
-    coarbitrary tile = coarbitrary (textureLoc tile, rules tile, charRep tile)
+    coarbitrary (Tile textureLoc rules charRep) = coarbitrary (textureLoc, rules, charRep)
 
 instance CoArbitrary Rule where
-    coarbitrary rule = coarbitrary (case rule of Rule f -> f)
+    coarbitrary (Rule rule) = coarbitrary rule
 
-prop_allPosArePlaced :: IO Property
-prop_allPosArePlaced = do
-    tiles <- generate arbitrary :: IO [Tile]
-    size <- generate arbitrary :: IO Size
+-- | Generate a size for the world, given a minimum and maximum size for the world
+genSize :: Size -> Gen Size
+genSize ((minX, minY, minZ), (maxX, maxY, maxZ)) = do
+    minX' <- choose (minX, maxX)
+    minY' <- choose (minY, maxY)
+    minZ' <- choose (minZ, maxZ)
+    maxX' <- choose (minX', maxX)
+    maxY' <- choose (minY', maxY)
+    maxZ' <- choose (minZ', maxZ)
+    return ((minX', minY', minZ'), (maxX', maxY', maxZ'))
+
+-- | Test if all positions are placed in the tilemap if the wave function collapse algorithm is run with a list of tiles 
+--   and a size for the world, where the rule for each tile is that it can be placed at any position.
+allPosArePlaced :: IO Property
+allPosArePlaced = do
+    tiles <- generate (listOf arbitrary) :: IO [Tile]
+    size <- generate (genSize ((0, 0, 0), (2, 2, 2))) :: IO Size
     (TileMap tileMap) <- waveFuncCollapse tiles size
     let ((minX, minY, minZ), (maxX, maxY, maxZ)) = size
     let allPos = [(x, y, z) | x <- [minX..maxX], y <- [minY..maxY], z <- [minZ..maxZ]]
     return $ forAll (elements allPos) $ \pos -> 
         M.member pos tileMap
+
