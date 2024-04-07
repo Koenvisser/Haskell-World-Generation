33bbebe5634599e853fa8006ae5a4a8f61c269e9
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2024-04-07 10:27:43 +0200

    remove internal library, use other-modules

--- src/Gen/WaveFuncCollapse.hs
+++ src/Gen/WaveFuncCollapse.hs
@@ -1,7 +1,7 @@
 -- | The wave function collapse algorithm is a way to generate a tilemap based on a set of rules.
 --   The algorithm works by starting with a completely empty tilemap and then collapsing the wave function
 --   of each tile based on the rules and the surrounding tiles. This is done until the entire tilemap is filled.
-module Gen.WaveFuncCollapse (waveFuncCollapse) where
+module Gen.WaveFuncCollapse (waveFuncCollapse, Error) where
 
 import Internal.Def (getVal, getPos)
 import Def
