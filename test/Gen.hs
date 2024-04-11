c523b774b3028472335909f1ff677110a2ef217b
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2024-04-11 19:58:21 +0200

    fix tests

--- test/Gen.hs
+++ test/Gen.hs
@@ -13,15 +13,16 @@ import Gen.WaveFuncCollapse
 test_WaveFuncCollapse :: [TestTree]
 test_WaveFuncCollapse = [
     testProperty "All positions are placed" (ioProperty allPosArePlaced),
-    testProperty "All rules are satisfied" (ioProperty allRulesAreSatisfied)
+    testProperty "All rules are satisfied"  (ioProperty allRulesAreSatisfied),
+    testProperty "Size is correct"          (ioProperty sizeIsCorrect)
     ]
 
 -- | Generate a size for the world, given a minimum and maximum size for the world
 genSize :: Size -> Gen Size
 genSize ((minX, minY, minZ), (maxX, maxY, maxZ)) = do
-    minX' <- choose (minX, maxX)
-    minY' <- choose (minY, maxY)
-    minZ' <- choose (minZ, maxZ)
+    minX' <- choose (minX , maxX)
+    minY' <- choose (minY , maxY)
+    minZ' <- choose (minZ , maxZ)
     maxX' <- choose (minX', maxX)
     maxY' <- choose (minY', maxY)
     maxZ' <- choose (minZ', maxZ)
@@ -44,7 +45,7 @@ allPosArePlaced = do
     result <- waveFuncCollapse tiles size
     case result of
         Left _ -> return $ property False
-        Right (TileMap tileMap) -> do
+        Right (TileMap (tileMap, _)) -> do
             -- Generate all positions in the world
             let ((minX, minY, minZ), (maxX, maxY, maxZ)) = size
             let allPos = [(x, y, z) | x <- [minX..maxX], y <- [minY..maxY], z <- [minZ..maxZ]]
@@ -62,17 +63,32 @@ allRulesAreSatisfied = do
     result <- waveFuncCollapse tiles size
     case result of
         Left _ -> return $ property False
-        Right (TileMap tileMap) -> do
+        Right (TileMap (tileMap, _)) -> do
             -- Test if all rules are satisfied
             return $ forAll (elements $ M.toList tileMap) $ \(pos, tile) -> 
                 let (Rule rule) = rules tile
-                    (RuleMonad ruleResult _) = rule (TileMap tileMap) pos
+                    (RuleMonad ruleResult _) = rule (TileMap (tileMap, size)) pos
                 in resultToBool ruleResult
+
+-- | Test if the size of the tilemap is correct if the wave function collapse algorithm is run with a list of tiles
+sizeIsCorrect :: IO Property
+sizeIsCorrect = do
+    -- Generate a list of tiles that is not empty
+    size <- generate (genSize ((0, 0, 0), (2, 2, 2))) :: IO Size
+    tiles <- generate $ genTiles size :: IO [Tile]
+    -- Execute the wave function collapse algorithm
+    result <- waveFuncCollapse tiles size
+    case result of
+        Left _ -> return $ property False
+        Right (TileMap (_,size')) -> do
+            -- Test if the size of the tilemap is correct
+            return $ size === size'
 
 -- | Generate a list of tiles, which uses the rule from `genRule`
 --   For each position in the world, there must be at least one tile that can be placed at that position
 genTiles :: Size -> Gen [Tile]
-genTiles ((minX, minY, minZ), (maxX, maxY, maxZ)) = listOf1 genTile `suchThat` (\tiles -> all (\pos -> any (\(Tile _ (Rule rule) _) -> let (RuleMonad result _) = rule (TileMap M.empty) pos in resultToBool result) tiles) positions)
+genTiles ((minX, minY, minZ), (maxX, maxY, maxZ)) = listOf1 genTile `suchThat` (\tiles -> all (\pos -> any (\(Tile _ (Rule rule) _) -> 
+    let (RuleMonad result _) = rule (TileMap (M.empty, ((0,0,0), (0,0,0)))) pos in resultToBool result) tiles) positions)
     where positions = [(x, y, z) | x <- [minX..maxX], y <- [minY..maxY], z <- [minZ..maxZ]]
 
 -- | Generate a tile, which uses the rule from `genRule`
