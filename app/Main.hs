cf56675533add2e6f1ded22707e680308845e684
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2024-04-12 10:14:47 +0200

    add save image to main

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import qualified Examples.Roads as Roads (allTiles)
 import qualified Examples.Mountains as Mountains (airTiles, groundTiles)
 import Gen.PerlinNoise (perlinNoiseRandom)
 import Gen.WaveFuncCollapse (waveFuncCollapse, waveFuncCollapeHeightMap)
-import Output (saveWorldToObjAndMtl)
+import Output (saveWorldToObjAndMtl, saveHeightMapToImage)
 
 main :: IO ()
 main = generateMountains
@@ -22,6 +22,7 @@ generateRoads = do
 generateMountains :: IO ()
 generateMountains = do
   heightMap <- perlinNoiseRandom def
+  saveHeightMapToImage heightMap 1000 1000 "output/image.png"
   result <- waveFuncCollapeHeightMap heightMap Mountains.airTiles Mountains.groundTiles ((0, 0, 0), (10, 8, 10))
   case result of
     Left err -> putStrLn err
